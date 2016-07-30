5d1b2356ec306464090f6dd278e03f6b1e4045cb
Sampsa Saari <sampsa.saari@gmail.com>
2016-07-30 21:24:00 +0300

    Navigation finished

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#mobile-menu').click(function(event) {
     $('.navbar').toggleClass('responsive');
+    $('#nav').toggleClass('responsive');
     console.log("vaihtu??");
   });
 });

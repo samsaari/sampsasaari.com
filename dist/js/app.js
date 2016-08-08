543d1286839b9501136b14b3c31f4ed9cbc5cc48
Sampsa Saari <sampsa.saari@gmail.com>
2016-08-08 10:57:54 +0300

    Added portfolio and footer sections. Deleted unused cover images.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,46 @@
-$(document).ready(function() {
-  $('#mobile-menu').click(function(event) {
-    $('.navbar').toggleClass('responsive');
-    $('#nav').toggleClass('responsive');
-    console.log("vaihtu??");
+$(function() {
+
+  var cache = {
+    $mobileMenu: $('#mobile-menu'),
+    $navbar: $('.navbar'),
+    $nav: $('#nav')
+
+  };
+
+  var app = {
+
+    init: function() {
+      app.navToggle();
+    },
+
+    navToggle: function() {
+      cache.$mobileMenu.on('click', function() {
+
+        cache.$nav.toggleClass('responsive');
+        cache.$navbar.toggleClass('responsive');
+
+        cache.$mobileMenu.find('i').toggleClass('fa-times fa-bars');
+  
+        return false;
+
+      });
+    }
+  };
+
+  app.init();
+
+  // Smooth Scrolling
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
+
 });
